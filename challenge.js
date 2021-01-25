080193ebf24f9d8df4753b57502356021b80b727
Madhura <madhura.bhujbal@gmail.com>
2021-01-25 14:31:12 +0000

    Add function to convert string into int and store it in an array

--- challenge.js
+++ challenge.js
@@ -1,14 +1,20 @@
-/**
-* The following is the function where the solution shall be written
-*/
 
 function solution (input) {
+    let numArray = parseInput(input);
 
     // logic here
-  
-    return null;
+     return numArray;
   }
-  
+
+  function parseInput (input) {
+    // convert all string charcters to number array elements
+    let numArr = [];
+    for(let char of input.split("")) {
+        numArr.push(parseInt(char));
+    }
+    return numArr;
+  };
+
   // some example inputs
-  console.log(solution('326')); // expected ouput 632,623,362,326,263,236
-  console.log(solution('A 3B2 C6D')); // expected ouput 632,623,362,326,263,236
+   console.log(solution('326')); // expected ouput 632,623,362,326,263,236
+//   console.log(solution('A 3B2 C6D')); // expected ouput 632,623,362,326,263,236
