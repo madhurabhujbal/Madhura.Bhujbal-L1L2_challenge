8a823235a940f2fe154757931a4c3b9b74ea7cc5
Madhura <madhura.bhujbal@gmail.com>
2021-01-25 22:41:57 +0000

    Add solution for challenge 1

--- challenge.js
+++ challenge.js
@@ -1,9 +1,10 @@
 
 function solution (input) {
-    let numArray = parseInput(input);
-
     // logic here
-     return numArray;
+    let numArray = parseInput(input);
+    let list = permutations(numArray);
+
+     return list;
   }
 
   function parseInput (input) {
@@ -14,6 +15,46 @@ function solution (input) {
     }
     return numArr;
   };
+
+  function fitNewNumber(currArr, pos, num) {
+    let newArr = [...currArr];
+    newArr.splice(pos, 0, num);
+    return newArr;
+}
+
+function arrayToNumber(list) {
+    let num = 0;
+    for(let i of list) {
+        num = (num * 10) + i;
+    }
+    return num;
+}
+
+function permutations(input) {
+    //edge cases for empty and single input
+    let lastNum = input[input.length - 1]
+    let list = [ [lastNum] ];
+    for(let i = 0; i < input.length - 1; i++) {
+        let currNum = input[i];
+        let newList = [];
+
+        for(let listItem of list) {
+            for(let j = 0; j <= listItem.length; j++) {
+                newElement = fitNewNumber(listItem, j, currNum);
+                newList.push(newElement);
+            }
+        }
+        list = newList;
+    }
+    //convert each item in the list to its n digit number equivalent
+    let result = [];
+    for(let item of list) {
+        result.push(arrayToNumber(item));
+    }
+
+    let sortedArray =  result.sort((a, b) => b - a);    //sort in elements descending order
+    return sortedArray.join(',');                       // return comma separated list of elements
+}
 
   // some example inputs
    console.log(solution('326')); // expected ouput 632,623,362,326,263,236
