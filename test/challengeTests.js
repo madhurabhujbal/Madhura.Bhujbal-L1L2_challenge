26f214dc414b137ecd4ce58fb07ba2285bdf329d
Madhura <madhura.bhujbal@gmail.com>
2021-01-26 12:41:45 +0000

    Add unittests - fails for single char input size

--- test/challengeTests.js
+++ test/challengeTests.js
@@ -7,4 +7,23 @@ describe('solution function', function () {
             assert.strictEqual(challenge.solution('326'), '632,623,362,326,263,236');
         });
     });
+
+    describe('When Passed alphanumeric string', function () {
+        it('should ignore the non-numeric characters and process the remaining digits', function () {
+            assert.strictEqual(challenge.solution('A 3B2 C6D'), '632,623,362,326,263,236');
+        });
+    });
+
+    describe('When Passed string with just one number', function () {
+        it('should should return the number', function () {
+            assert.strictEqual(challenge.solution('3'), '3');
+        });
+    });
+
+    describe('When Passed alphabet string', function () {
+        it('should give an error message', function () {
+            assert.strictEqual(challenge.solution('A B CD'), "No valid number found in the given input => 'A B CD'");
+        });
+    });
+
 });
