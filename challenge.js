978b4282488ac8e8cf0577c0f594a0f7cad5c7b6
Madhura <madhura.bhujbal@gmail.com>
2021-01-26 12:49:11 +0000

    Reset to run the given input

--- challenge.js
+++ challenge.js
@@ -62,8 +62,8 @@ function calcPermutations(input) {
 }
 
   // some example inputs
-    //console.log(solution('326')); // expected ouput 632,623,362,326,263,236
-    console.log(solution('4')); // expected ouput 4
+    console.log(solution('326')); // expected ouput 632,623,362,326,263,236
+    // console.log(solution('4')); // expected ouput 4
     //   console.log(solution('A 3B2 C6D')); // expected ouput 632,623,362,326,263,236
     //   console.log(solution('A B CD')); // expected ouput 632,623,362,326,263,236
 
